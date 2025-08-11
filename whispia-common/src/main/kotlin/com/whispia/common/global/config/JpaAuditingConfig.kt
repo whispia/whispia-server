8d4c796fcdf8552edfa416ee488a9976c64dc5d8
Seungwon-Choi <bestwondev@gmail.com>
2025-08-11 23:23:19 +0900

    feat: BaseTimeEntity 클래스 추가 #13

---
+++ whispia-common/src/main/kotlin/com/whispia/common/global/config/JpaAuditingConfig.kt
@@ -0,0 +1,8 @@
+package com.whispia.common.global.config
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableJpaAuditing // JPA Auditing 기능을 활성화합니다.
+class JpaAuditingConfig {
+}
