58e215f3b177d987a94ec13c062997e094865476
Seungwon-Choi <bestwondev@gmail.com>
2025-08-11 23:47:01 +0900

    feat: EnableJpaAuditing 임포트문 추가 #13

--- whispia-common/src/main/kotlin/com/whispia/common/global/config/JpaAuditingConfig.kt
+++ whispia-common/src/main/kotlin/com/whispia/common/global/config/JpaAuditingConfig.kt
@@ -1,8 +1,9 @@
 package com.whispia.common.global.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @Configuration
-@EnableJpaAuditing // JPA Auditing 기능을 활성화합니다.
+@EnableJpaAuditing
 class JpaAuditingConfig {
 }
