f3a5c02f8895100dc2df2ac15b792761cf9ecb77
JaeHyeon Yoo <jhyoo1203@gmail.com>
2025-08-15 23:28:32 +0900

    feat: 생성자 private 접근 제어

--- whispia-common/src/main/kotlin/com/whispia/common/global/dto/ResponseData.kt
+++ whispia-common/src/main/kotlin/com/whispia/common/global/dto/ResponseData.kt
@@ -5,11 +5,22 @@ import java.time.LocalDateTime
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class ResponseData<T>(
-    val timestamp: LocalDateTime = LocalDateTime.now(),
+    val timestamp: LocalDateTime,
     val httpStatus: Int,
-    val errorCode: ErrorCode? = null,
-    val data: T? = null
+    val data: T? = null,
+    val errorCode: ErrorCode? = null
 ) {
+    private constructor(
+        httpStatus: Int,
+        data: T? = null,
+        errorCode: ErrorCode? = null
+    ) : this(
+        timestamp = LocalDateTime.now(),
+        httpStatus = httpStatus,
+        data = data,
+        errorCode = errorCode
+    )
+
     companion object {
         fun <T> of(
             httpStatus: Int,
